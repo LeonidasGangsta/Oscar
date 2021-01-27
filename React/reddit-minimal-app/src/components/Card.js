e8423592a992dfa4d8c5bdbba0a74e781c2543a0
LeonidasGangsta <oscardtorres@hotmail.com>
2021-01-27 01:10:26 -0500

    Styles changes

--- React/reddit-minimal-app/src/components/Card.js
+++ React/reddit-minimal-app/src/components/Card.js
@@ -1,5 +1,8 @@
 import { useSelector } from 'react-redux';
 import './styles/css/Card.css'
+import commentIcon from '../Icons/comment-icon.svg';
+import arrowUp from '../Icons/arrow-up.svg';
+import arrowDown from '../Icons/arrow-down.svg';
 
 const Card = () => {
 
@@ -8,15 +11,31 @@ const Card = () => {
     return (
         <div className='cardContainer'>
             <div className='cardTitle'>
-                <h3>{card.title}</h3>
+                <h4>{card.title}</h4>
             </div>
-            <div className='imgContainer'>
-                <img src={card.img} alt=''/>
+            <div className='cardBody'>
+                <div className='arrows'>
+                    <img src={arrowUp} alt=''/>
+                    <div className='count'>
+                        <p>350k</p>
+                    </div>
+                    <img src={arrowDown} alt=''/>
+                </div>
+                <div className='imgContainer'>
+                    <img src={card.img} alt=''/>
+                </div>
             </div>
+            <hr/>
             <div className='cardInteractions'>
-                <p>Posted by {card.author}</p>
+                <div className='author'>
+                    <p>Posted by</p>
+                    <p style={{color: '#55C0FD'}}>{card.author}</p>
+                </div>
                 <p>{card.old} hours ago</p>
-                <p>{card.comments} comments</p>
+                <div className='comments'>
+                    <img src={commentIcon} alt=''/>
+                    <p>{card.comments} comments</p>
+                </div>
             </div>
         </div>
     )
